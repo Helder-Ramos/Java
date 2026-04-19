f90cea81b3d4dc1601b5a9c83277c47cc61200d2
Helder de Sousa Ramos <helder.sramos@gmail.com>
2026-04-19 19:39:29 -0300

    Atualização dos arquivos

---
+++ Deitel/src/capitulo02/Welcome2.java
@@ -0,0 +1,14 @@
+// Figura 2.3: Welcome2.java
+// Imprimindo uma linha de texto com múltiplas instruções.
+
+package capitulo02;
+
+public class Welcome2
+{
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args)
+    {
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+    } // fim do método main
+} // fim da classe Welcome2
